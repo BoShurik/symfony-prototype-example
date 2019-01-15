a593fa078b80077d816dd3e987bea28be01f39a8
BoShurik <boshurik@gmail.com>
2019-01-15 14:00:43 +0300

    Add additional data (user and currency)

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -9,7 +9,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/order")
+ * @Route("/")
  */
 class OrderController extends AbstractController
 {
